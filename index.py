605f8e2785582ee532927e930424e6405db74956
thangvb <bathangvu@gmail.com>
2024-01-04 09:00:22 +0700

    UPDATE: config show image

--- index.py
+++ index.py
@@ -7,8 +7,12 @@ landmarks = ms.getLandmark(image)
 x = image.shape[1]
 y = image.shape[0]
 
+print(x, y)
+
 cv2.line(image, ms.getHeadPoint(image), ms.getAvgHeel(landmarks, x, y), (0, 225, 0), 3)
 
-image = cv2.resize(image, (0, 0), fx=0.2, fy=0.2)
+ratio_image = 500 / y
+
+image = cv2.resize(image, (0, 0), fx=ratio_image, fy=ratio_image)
 cv2.imshow('Result', image)
 cv2.waitKey(0)
