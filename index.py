d8f801bf48470d7f4e609d407640ae3786cc9611
tphuyen <trinhhuyen.tph@gmail.com>
2024-01-06 00:43:10 +0700

    UPDATE: print getDistances

--- index.py
+++ index.py
@@ -9,10 +9,6 @@ y = image.shape[0]
 
 print(x, y)
 
-# Vẽ tất cả các điểm trên ảnh
-for point in ms.getAvgHeel(landmarks, x, y)[2]:
-    cv2.circle(image, tuple(point), 5, (0, 255, 0), -1)  # Vẽ một hình tròn với bán kính 5 tại mỗi điểm
-
 cv2.line(image, ms.getHeadPoint(image), ms.getAvgHeel(landmarks, x, y)[:2], (0, 255, 0), 3)
 
 ratio_image = 500 / y
@@ -20,6 +16,5 @@ ratio_image = 500 / y
 image = cv2.resize(image, (0, 0), fx=ratio_image, fy=ratio_image)
 cv2.imshow('Result', image)
 cv2.waitKey(0)
+print("Khoảng cách:", ms.getDistances(landmarks))
 
-result_space_shoulder = spaceBody(left_shoulder, right_shoulder)
-print("Space between shoulders:", result_space_shoulder)
