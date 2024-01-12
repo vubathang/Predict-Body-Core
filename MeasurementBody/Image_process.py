cf16602dcd4057367ca104b34c25b295b594d217
hungbeoqua321 <69001944+hungbeoqua321@users.noreply.github.com>
2024-01-12 09:20:12 +0700

    Revert "hihi"
    This reverts commit d131b8f4aab4c3b1c2c8d35b7cd276e9b7c98076.

--- MeasurementBody/Image_process.py
+++ MeasurementBody/Image_process.py
@@ -9,7 +9,6 @@ def getCannyEdge(image):
   contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   return contours
 
-
 canny_image = getCannyEdge(image)
 
 def getCoordiantes(image):
